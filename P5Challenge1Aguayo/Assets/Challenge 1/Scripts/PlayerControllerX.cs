1d161017c3e1153a44249a4560d3e7f25e2bb2a2
janelle <731364@student.dusd.net>
2024-03-06 13:48:26 -0800

    i fixed most of the problems
    i made the plane go forward, i can make the plane go up and down with the arrows, i fixed the position of the camera and made it follow the plane, and i fixed the speed of the plane

---
+++ P5Challenge1Aguayo/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed;
+    private float rotationSpeed = 40.0f;
+    public float verticalInput;
+    public float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        
+        transform.Translate(Vector3.forward * speed);
+
+        transform.Rotate(Vector3.up, rotationSpeed * horizontalInput * Time.deltaTime);
+        transform.Rotate(Vector3.left, rotationSpeed * verticalInput * Time.deltaTime);
+    }
+}
